1c3eb9fd7406b4d2ded13050cf794996f89542d6
Omri <110406612+OmriPy@users.noreply.github.com>
2023-02-22 10:57:47 +0200

    Update server.py

--- 4.4/server.py
+++ 4.4/server.py
@@ -1,10 +1,8 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import os
 
-PORT = 80
-
-def read_file(file_name: str) -> bytes:
-    with open(file_name, 'rb') as file:
+def read_file(file_path: str) -> bytes:
+    with open(file_path, 'rb') as file:
         return file.read()
 
 def handle_client(client: socket):
@@ -12,20 +10,23 @@ def handle_client(client: socket):
     if not (data[0].startswith('GET /') and data[0].split(' ')[2] == 'HTTP/1.1'):
         client.close()
         return
-    print('Valid packet')
+    print('Valid packet\n')
     url_name = data[0].split(' ')[1]
     if url_name == '/':
         url_name += 'index.html'
     url_name = f"{os.getcwd()}{os.path.sep}webroot{url_name}"
+    if not os.path.isfile(url_name):
+        client.close()
+        return
     client.send('HTTP/1.1 200 OK\r\n\r\n'.encode() + read_file(url_name))
     client.close()
 
 def server():
     with socket(AF_INET, SOCK_STREAM) as server:
         try:
-            server.bind(('0.0.0.0', PORT))
+            server.bind(('0.0.0.0', 80))
         except OSError as e:
-            print(f'{e}\nTry again later')
+            print('Try again later')
             return
         print("Server is running")
         server.listen()
